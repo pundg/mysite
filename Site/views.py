f36c00284340a46145751cdc3b9a07d7309bf5da
Gaurav Pund <gauravpund5464@gmail.com>
2021-07-12 15:24:42 +0530

    first project django

--- Site/views.py
+++ Site/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def Welcome(request):
-    return HttpResponse("hello weclome the Mywebsite")
+    return HttpResponse("hello welcome the Mywebsite")
